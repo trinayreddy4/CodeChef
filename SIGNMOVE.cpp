4e7c6c02adc45d0293dc5e64f82c9661768fa8ce
Trinay Reddy <Trinayreddy4@gmail.com>
2023-09-18 09:05:09 +0530

    .

---
+++ SIGNMOVE.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define nl endl
+#define ou cout
+#define in cin
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define yes ou<<"YES"<<nl;
+#define no ou<<"NO"<<nl;
+using ll=long long;
+using lli=long long int;
+using namespace std;
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<string> vs;
+int main()
+{
+    fastread();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		in>>n;
+		
+		if(n%2==0)
+		ou<<n/2<<nl;
+		else
+		ou<<-(n/2 +1)<<nl;
+	}
+}
+
+
